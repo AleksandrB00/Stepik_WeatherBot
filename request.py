dd80dbdc9075dd33b8d658cc061000b163b71c70
AleksandrB00 <babaev_aleksandr@icloud.com>
2022-09-05 17:41:47 +0200

    ццц

--- request.py
+++ request.py
@@ -8,6 +8,6 @@ def get_city_coord(city):
     payload = {'geocode' : city, 'apikey' : config.geo_key, 'format' : 'json'}
     r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
     geo = json.loads(r.text)
-    return print(geo['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'])
+    return geo['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
 
 get_city_coord('Калининград')
