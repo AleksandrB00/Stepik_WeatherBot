2bad49040e016b7b128fa4eccb8b0611fec5d44d
AleksandrB00 <babaev_aleksandr@icloud.com>
2022-09-06 17:50:35 +0200

    цфква

--- database/models.py
+++ database/models.py
@@ -1,9 +1,35 @@
-from sqlalchemy import Column, Integer, String, DateTime,  ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base 
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
 class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
-    
+    title = Column(String(60), nullable=False)
+    author = Column(String(30), nullable=False)
+    reviews = relationship('Rewiews', backref='book', lazy=True)
+
+    def __repr__(self):
+        return self.title
+
+class Reviews(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    text = Column(String(2000), nullable=False)
+    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f'От {self.reviewer}'
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(20), nullable=False)
+    reviews = relationship('Reviews', backref='reviewer', lazy=True)
+
+    def __repr__(self):
+        return self.name
