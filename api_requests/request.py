093849ce1b7b8aef5924b09f4f45533813e35f25
AleksandrB00 <babaev_aleksandr@icloud.com>
2022-09-06 17:18:26 +0200

    database

---
+++ api_requests/request.py
@@ -0,0 +1,18 @@
+import json 
+
+import requests
+
+from ..settings import api_config
+
+def get_city_coord(city):
+    payload = {'geocode' : city, 'apikey' : api_config.geo_key, 'format' : 'json'}
+    r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
+    geo = json.loads(r.text)
+    return geo['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+
+def get_weather(city):
+    coordinates = get_city_coord(city).split()
+    payload = {'lat' : coordinates[1], 'lon' : coordinates[0], 'lang' : 'ru_RU'}
+    r = requests.get('https://api.weather.yandex.ru/v2/forecast', params=payload, headers=api_config.weather_key)
+    weather_data = json.loads(r.text)
+    return weather_data
---
+++ database/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String, DateTime,  ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key=True)
+    
