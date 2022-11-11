9c86d45d07ecf0382a40ff41a3312fcd95bc31f5
AleksandrB00 <104912130+AleksandrB00@users.noreply.github.com>
2022-11-11 15:38:45 +0500

    Update orm.py

--- database/orm.py
+++ database/orm.py
@@ -4,10 +4,11 @@ from .models import Base, User, WeatherReport
 
 from settings import database_config
 
+engine = create_engine(database_config.url, echo=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+    
 def add_user(tg_id):
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     user = session.query(User).filter(User.tg_id == tg_id).first()
     if user is None:
@@ -16,18 +17,12 @@ def add_user(tg_id):
         session.commit()
 
 def set_user_city(tg_id, city):
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     user = session.query(User).filter(User.tg_id == tg_id).first()
     user.city = city
     session.commit()
 
 def create_report(tg_id, temp, feels_like, wind_speed, pressure_mm, city):
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     user = session.query(User).filter(User.tg_id == tg_id).first()
     new_report = WeatherReport(temp=temp, feels_like=feels_like, wind_speed=wind_speed, pressure_mm=pressure_mm, city=city, owner=user.id)
@@ -35,35 +30,23 @@ def create_report(tg_id, temp, feels_like, wind_speed, pressure_mm, city):
     session.commit()
 
 def get_user_city(tg_id):
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     user = session.query(User).filter(User.tg_id == tg_id).first()
     return user.city
 
 def get_reports(tg_id):
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     user = session.query(User).filter(User.tg_id == tg_id).first()
     reports = user.reports
     return reports
 
 def delete_user_report(report_id):
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     report = session.get(WeatherReport, report_id)
     session.delete(report)
     session.commit()
 
 def get_all_users():
-    engine = create_engine(database_config.url, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
     session = Session()
     users = session.query(User).all()
     return users
