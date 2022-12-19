56552d973688ee6d92c2b1962b95c3f56e8085ca
AleksandrB00 <104912130+AleksandrB00@users.noreply.github.com>
2022-12-19 15:20:06 +0500

    Update request.py

--- api_requests/request.py
+++ api_requests/request.py
@@ -6,14 +6,14 @@ from settings import api_config
 
 
 def get_city_coord(city):
-    payload = {'geocode' : city, 'apikey' : api_config.geo_key, 'format' : 'json'}
+    payload = {'geocode': city, 'apikey': api_config.geo_key, 'format': 'json'}
     r = requests.get('https://geocode-maps.yandex.ru/1.x', params=payload)
     geo = json.loads(r.text)
     return geo['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
 
 def get_weather(city):
     coordinates = get_city_coord(city).split()
-    payload = {'lat' : coordinates[1], 'lon' : coordinates[0], 'lang' : 'ru_RU'}
-    r = requests.get('https://api.weather.yandex.ru/v2/informers', params=payload, headers=api_config.weather_key)
+    payload = {'lat': coordinates[1], 'lon': coordinates[0], 'lang': 'ru_RU'}
+    r = requests.get('https://api.weather.yandex.ru/v2/forecast', params=payload, headers=api_config.weather_key)
     weather_data = json.loads(r.text)
     return weather_data['fact']
